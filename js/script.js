0bff40520ffcc5b976fe9f804e79e9779a9c6a87
marcelogonsalvess-bit <marcelogonsalvess@gmail.com>
2026-06-28 18:58:00 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector(".slider");
+
+document.querySelector(".next").onclick = () => {
+
+    slider.scrollBy({
+
+        left: 600,
+
+        behavior: "smooth"
+
+    });
+
+}
+
+document.querySelector(".prev").onclick = () => {
+
+    slider.scrollBy({
+
+        left: -600,
+
+        behavior: "smooth"
+
+    });
+
+}
+
