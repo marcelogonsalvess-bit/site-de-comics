deabe0a45218a849d9659cc42d73d800453fc93e
marcelogonsalvess-bit <marcelogonsalvess@gmail.com>
2026-06-29 13:30:02 -0300

    Revert "menu sanduiche"
    This reverts commit c6dc824daa635c7097131b0b8f26f65f68315938.

--- js/script.js
+++ js/script.js
@@ -84,10 +84,3 @@ function atualizarHero() {
 
 
 
-
-const toggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
-
-toggle.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
