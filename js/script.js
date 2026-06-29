2c4e2512f1c833365b79eacc209d948e1979fb18
marcelogonsalvess-bit <marcelogonsalvess@gmail.com>
2026-06-29 03:27:34 -0300

    contato

--- js/script.js
+++ js/script.js
@@ -85,3 +85,54 @@ function atualizarHero() {
 
 
 
+const modal = document.getElementById("modalContato");
+const openBtn = document.getElementById("openContato");
+const closeBtn = document.getElementById("closeContato");
+
+const form = document.getElementById("formContato");
+const statusMsg = document.getElementById("statusMsg");
+
+// abre / fecha com toggle
+openBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.toggle("ativo");
+});
+
+// fecha botão X
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("ativo");
+});
+
+// fecha clicando fora
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.classList.remove("ativo");
+    }
+});
+
+// envio
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    try {
+        const res = await fetch("https://formsubmit.co/ajax/marcelo@marcelogonsalves.com.br", {
+            method: "POST",
+            body: data
+        });
+
+        if (res.ok) {
+            statusMsg.innerText = "Mensagem enviada com sucesso!";
+            form.reset();
+
+            // fecha modal após enviar
+            modal.classList.remove("ativo");
+        } else {
+            statusMsg.innerText = "Erro ao enviar.";
+        }
+
+    } catch (err) {
+        statusMsg.innerText = "Erro de conexão.";
+    }
+});
