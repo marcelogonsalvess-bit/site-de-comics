c6dc824daa635c7097131b0b8f26f65f68315938
marcelogonsalvess-bit <marcelogonsalvess@gmail.com>
2026-06-29 05:55:16 -0300

    menu sanduiche

--- js/script.js
+++ js/script.js
@@ -84,3 +84,10 @@ function atualizarHero() {
 
 
 
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
