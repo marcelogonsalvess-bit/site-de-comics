71be9d8fadf76d0158ee8f7503061459adde66f4
MarceloCel <marcelogonsalvess@gmail.com>
2026-08-14 12:47:23 -0300

    marvel knights editado

--- pages/admin2/js/admin2.js
+++ pages/admin2/js/admin2.js
@@ -41,3 +41,97 @@ menuItems.forEach(item => {
     });
 
 });
+
+// =====================================
+// VISUALIZAR USUÁRIO
+// =====================================
+
+function visualizarUsuario(botao) {
+
+    const linha = botao.closest("tr");
+
+    const nome = linha.querySelector(".admin-table-user-name").textContent.trim();
+
+    const email = linha.querySelector(".admin-table-user-email").textContent.trim();
+
+    const status = linha.querySelector(".admin-table-status").textContent.trim();
+
+    const cadastro = linha.querySelector("td:nth-child(3)").textContent.trim();
+
+    console.log("Nome:", nome);
+    console.log("E-mail:", email);
+
+    document.getElementById("modalUsuarioNome").textContent = nome;
+
+    document.getElementById("modalUsuarioEmail").textContent = email;
+
+    document.getElementById("modalUsuarioStatus").textContent = status;
+
+    const statusElemento = document.getElementById(
+        "modalUsuarioStatus"
+    );
+
+    statusElemento.classList.remove("ativo", "inativo");
+
+    statusElemento.classList.add(
+        status.toLowerCase()
+    );
+
+    document.getElementById("modalUsuarioCadastro").textContent = cadastro;
+
+    const modal = document.getElementById("modalVisualizarUsuario");
+
+    modal.classList.add("active");
+
+}
+
+
+document.addEventListener("click", function (event) {
+
+    const botaoVisualizar = event.target.closest(
+        '[data-action="visualizar-usuario"]'
+    );
+
+    if (!botaoVisualizar) return;
+
+    visualizarUsuario(botaoVisualizar);
+
+});
+
+// =====================================
+// FECHAR MODAL — VISUALIZAR USUÁRIO
+// =====================================
+
+const botaoFecharModal = document.getElementById(
+    "fecharModalVisualizarUsuario"
+);
+
+botaoFecharModal.addEventListener("click", function () {
+
+    const modal = document.getElementById(
+        "modalVisualizarUsuario"
+    );
+
+    modal.classList.remove("active");
+
+});
+
+lucide.createIcons();
+
+// =====================================
+// FECHAR MODAL — CLIQUE FORA
+// =====================================
+
+const modalVisualizarUsuario = document.getElementById(
+    "modalVisualizarUsuario"
+);
+
+modalVisualizarUsuario.addEventListener("click", function (event) {
+
+    if (event.target === modalVisualizarUsuario) {
+
+        modalVisualizarUsuario.classList.remove("active");
+
+    }
+
+});
